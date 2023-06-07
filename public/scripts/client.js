6ef0de61946a39f3ef8cce1326fb9df1d0b5cdf9
phankons <stephen.han@gmx.com>
2023-06-07 03:01:57 -0400

    almost done dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function (e) {
+
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  $("#tweets-container").append($tweet);
+
+  let maxCharacters = 140;
+
+  $("#tweet-text").value = "";
+
+  $("#tweet-text").on('input', function (text) {
+
+    // for example if you delete everything. 
+    if (this.textLength === 0) {
+
+      // reset counter
+      maxCharacters = 140;
+
+      // reset styling
+      $("#tweet-text").first().next().find("output").css("color", "#545149");
+    }
+
+    maxCharacters -= this.textLength;
+
+    // change the value of <output> </output> 
+    $("#tweet-text").next().find("output").text(maxCharacters);
+
+    if (maxCharacters <= 0) {
+      console.log("Less than 0, change formatting")
+      // Output pointer to modify CSS
+      $("#tweet-text").next().find("output").css("color", "red");
+    }
+  });
+
+ 
+ 
+});
+
+
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -8,6 +8,7 @@ const tweetsRoutes = express.Router();
 module.exports = function (DataHelpers) {
 
   tweetsRoutes.get("/", function (req, res) {
+    
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -19,6 +20,7 @@ module.exports = function (DataHelpers) {
 
   tweetsRoutes.post("/", function (req, res) {
 
+    
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body' });
       return;
@@ -44,8 +46,6 @@ module.exports = function (DataHelpers) {
 
 
   tweetsRoutes.post("/tweets", function (req, res) {
-
-    console.log("HEREE");
     
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body' });
